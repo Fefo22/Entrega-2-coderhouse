256ac5f9e4a00cab0f62392371dd2d1ea1911688
Federico Micca <110429542+Fefo22@users.noreply.github.com>
2023-03-15 16:59:58 -0300

    Add files via upload

--- ENTREGA/js/main.js
+++ ENTREGA/js/main.js
@@ -1,94 +1,49 @@
-
-function sumar(a, b) {
-  return a + b;
-}
-
 let participantes = [];
-let num = parseInt(prompt("Cuántos participantes quieres ingresar?"));
 
-for (let i = 0; i < num; i++) {
-  let nombre = prompt("Ingresa el nombre de un participante ");
-  participantes.push(nombre);
+const ul = document.querySelector('ul');
+const form = document.querySelector('form');
+const quitarBtn = document.querySelector('#quitar');
+
+// Recuperar los nombres de los participantes de Local Storage al cargar la página
+if (localStorage.getItem('participantes')) {
+  participantes = JSON.parse(localStorage.getItem('participantes'));
+  participantes.forEach((nombre) => {
+    const li = document.createElement('li');
+    li.textContent = nombre;
+    ul.appendChild(li);
+  });
 }
 
-let contador = 0;
-let resultados = {};
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const input = document.querySelector('#nombre');
+  const nombre = input.value.trim();
 
-while (contador < 5) {
-  let participanteElegido = participantes[Math.floor(Math.random() * participantes.length)];
-  alert("El participante elegido es " + participanteElegido);
-
-  let numeroAleatorio = Math.ceil(Math.random() * 3);
-
-  const prenda1 = "tomar un fondo blanco";
-  const prenda2 = "llamar a un familiar y pedir dos empanadas de pollo";
-  const prenda3 = "reaccionar a la historia número 10 que te salga en Instagram";
-
-  let prendaAleatoria;
-
-  switch (numeroAleatorio) {
-    case 1:
-      prendaAleatoria = prenda1;
-      break;
-    case 2:
-      prendaAleatoria = prenda2;
-      break;
-    case 3:
-      prendaAleatoria = prenda3;
-      break;
+  // Verificar 
+  if (nombre !== '' && typeof nombre === 'string') {
+    participantes.push(nombre);
+    localStorage.setItem('participantes', JSON.stringify(participantes));
+    const li = document.createElement('li');
+    li.textContent = nombre;
+    ul.appendChild(li);
   }
 
-  let respuesta = prompt("El participante " + participanteElegido + " tiene que " + prendaAleatoria + ". ¿Lo haces?");
+  input.value = '';
+});
 
-  if (respuesta === "no") {
-    contador++;
-    if (resultados[participanteElegido]) {
-      resultados[participanteElegido].no++;
-    } else {
-      resultados[participanteElegido] = { si: 0, no: 1 };
-    }
-  } else if (respuesta === "si") {
-    if (resultados[participanteElegido]) {
-      resultados[participanteElegido].si++;
-    } else {
-      resultados[participanteElegido] = { si: 1, no: 0 };
-    }
-  } else {
-    alert("Por favor ingresa 'si' o 'no'");
+quitarBtn.addEventListener('click', () => {
+  if (participantes.length > 0) {
+    participantes.pop();
+    localStorage.setItem('participantes', JSON.stringify(participantes));
+    ul.removeChild(ul.lastChild);
   }
-}
+});
 
-alert("Un participante ha respondido 5 veces 'no', el juego ha terminado.");
+const ruletaImg = document.querySelector('.img-ruleta');
+const tirarBtn = document.querySelector('.btn-tirar');
 
-// Calcular la suma de los resultados
-let sumaSi = 0;
-let sumaNo = 0;
-for (let participante in resultados) {
-  sumaSi = sumar(sumaSi, resultados[participante].si);
-  sumaNo = sumar(sumaNo, resultados[participante].no);
-}
-
-// Mostrar resultados 
-document.write("<h2>Resultados:</h2>");
-document.write(`<p>Total: Sí ${sumaSi} / No ${sumaNo}</p>`);
-for (let participante in resultados) {
-  document.write(`<p>${participante}: Sí ${resultados[participante].si} / No ${resultados[participante].no}</p>`);
-}
-
-// Crear Prendas
-
-class Prenda{
-  constructor(nombre, dificultad, indicacion){
-      this.nombre = nombre;
-      this.dificultad = dificultad;
-      this.indicacion = indicacion;
-  }
-}
-
-const prenda4 = new Prenda("Beso a Beso", "3", "Dale un beso en la boca a cada jugador.");
-const prenda5 = new Prenda("T Rex", "1", "Girá 6 veces con las manos en posición de T-Rex. Si, leíste bien, mostrá ese dinosaurio interior.");
-const prenda6 = new Prenda("Stalker", "2", "Poné un Me gusta en una foto vieja de Instagram a una persona al azar que indique algún oponente.");
-
-
-
-
+tirarBtn.addEventListener('click', () => {
+  const randomNum = Math.floor(Math.random() * 16) + 1;
+  ruletaImg.classList.remove('girando1', 'girando2', 'girando3', 'girando4', 'girando5', 'girando6', 'girando7', 'girando8', 'girando9', 'girando10', 'girando11', 'girando12', 'girando13', 'girando14', 'girando15', 'girando16');
+  ruletaImg.classList.add(`girando${randomNum}`);
+});
